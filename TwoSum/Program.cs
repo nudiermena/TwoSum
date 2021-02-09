6599f835039129757867c3bb734805b6d62e4d7f
Nudier Mena <nmena@raptortech.com>
2021-02-09 09:23:23 -0500

    Agregar archivos de proyecto.

---
+++ TwoSum/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace TwoSum
+{
+    class Program
+    {
+        static int Target = 6;
+        static string Result;
+        static int[] Inputarray = { 1, 2, 3, 4, 5, 6 };
+        static void Main(string[] args)
+        {           
+            FindTarget(Inputarray);
+        }
+
+        static string FindTarget(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[i] + array[j] == Target)
+                    {                        
+                        Result = i.ToString() + " ," + j.ToString();
+                    }
+                }
+            }
+
+            return Result;
+        }
+    }
+}
